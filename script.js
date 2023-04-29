6f82d40623499a08a4e7760ddcecfc38eee4748e
nicole-nam <eunjungnam@gmail.com>
2023-04-28 23:14:12 -0400

    Added functionality to the form

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const formElement = document.querySelector("form");
+
+    formElement.addEventListener("submit", (e)=>{
+        
+        e.preventDefault();
+
+        const messageElement = document.querySelector('.form-message');
+        const message = "Thank you for your email!";
+
+        messageElement.innerHTML = message;
+        messageElement.style.display = "block";
+        formElement.reset();
+    });
+
+
+});
