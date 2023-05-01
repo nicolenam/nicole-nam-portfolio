e8253fc985b86889798fad2f26a3a958c597b5a7
nicole-nam <eunjungnam@gmail.com>
2023-04-30 22:29:16 -0400

    Added hamburger menu

--- script.js
+++ script.js
@@ -15,4 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
+    const hamburger = document.querySelector('.nav__hamburger');
+    const menu = document.querySelector('.nav__menu');
+    const close = document.querySelector(('.nav__close'));
+
+    hamburger.addEventListener("click", toggleMenu);
+    close.addEventListener("click", toggleMenu);
+
+    function toggleMenu(){
+        hamburger.classList.toggle('hide');
+        close.classList.toggle('show');
+        menu.classList.toggle('show');
+    }
+
 });
